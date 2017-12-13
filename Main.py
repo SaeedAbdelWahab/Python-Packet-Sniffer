739a174fbbd4a513a6e8e46cccda832551f1819c
SherifAbood <sherif.abood.fawzy@gmail.com>
2017-12-13 23:22:30 +0200

    treewidget instead of table

--- Main.py
+++ Main.py
@@ -8,8 +8,16 @@ from struct import *
 import datetime
 import pcapy
 import sys
+import time
 
-
+Row=0
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
 
 def selectTrigger():
     if(ui_home.DeviceList.currentRow()==-1):
@@ -25,9 +33,11 @@ def statueStart():
     ui_main.statusBar.showMessage("Sniffing...")
     sniffing = True
     cap = pcapy.open_live( str(SelectedDevice), 65536 , 1 , 0)
+    for i in range(10):
+        time.sleep(1)
+        (header, packet) = cap.next()
+        parse_packet(packet)
     
-    (header, packet) = cap.next()
-    parse_packet(packet)
 
 def eth_addr (a) :
     b = "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (ord(a[0]) , ord(a[1]) , ord(a[2]), ord(a[3]), ord(a[4]) , ord(a[5]))
@@ -35,15 +45,23 @@ def eth_addr (a) :
 
 def parse_packet(packet) :
     eth_length = 14
-     
+    global Row
     eth_header = packet[:eth_length]
     eth = unpack('!6s6sH' , eth_header)
     eth_protocol = socket.ntohs(eth[2])
-    rowPosition = ui_main.PacketTable.rowCount()
-    ui_main.PacketTable.insertRow(rowPosition)
-    ui_main.PacketTable.setItem(rowPosition,0,QtGui.QTableWidgetItem(str(eth_protocol)))
-    ui_main.PacketTable.setItem(rowPosition , 1, QtGui.QTableWidgetItem(eth_addr(packet[0:6])))
-    ui_main.PacketTable.setItem(rowPosition , 2, QtGui.QTableWidgetItem(eth_addr(packet[6:12])))
+    #rowPosition = ui_main.PacketTable.rowCount()
+    #ui_main.PacketTable.insertRow(rowPosition)
+    #ui_main.PacketTable.setItem(rowPosition,0,QtGui.QTableWidgetItem(str(eth_protocol)))
+    #ui_main.PacketTable.setItem(rowPosition , 1, QtGui.QTableWidgetItem(eth_addr(packet[0:6])))
+    #ui_main.PacketTable.setItem(rowPosition , 2, QtGui.QTableWidgetItem(eth_addr(packet[6:12])))
+    item_0 = QtGui.QTreeWidgetItem(ui_main.PacketTable)
+    ui_main.PacketTable.topLevelItem(Row).setText(0, _translate("MainWindow", str(eth_protocol), None))
+    ui_main.PacketTable.topLevelItem(Row).setText(1, _translate("MainWindow", eth_addr(packet[0:6]), None))
+    ui_main.PacketTable.topLevelItem(Row).setText(2, _translate("MainWindow", eth_addr(packet[6:12]), None))
+    Row+=1
+    #self.PacketTable.topLevelItem(rowPosition).setText(3, _translate("MainWindow", "field3", None))
+    #elf.PacketTable.topLevelItem(rowPosition).setText(4, _translate("MainWindow", "field4", None))
+
     #ui_main.PacketTable.setItem(rowPosition , 3, QtGui.QTableWidgetItem(eth_protocol))
     #sys.exit(1)
 
