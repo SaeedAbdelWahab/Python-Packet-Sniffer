5f54029c5064a54fe385b12e63a0f13fb2fe7a22
SaeedAbdelWahab <saeed.abdellwahab@gmail.com>
2017-12-14 05:49:34 +0200

    Multithreaidng problem solved git add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -A!

--- Main.py
+++ Main.py
@@ -3,15 +3,41 @@ import os,sys,pcapy
 from Windows.homewin import Ui_HomeWindow
 from Windows.MainWindow import Ui_MainWindow
 from PyQt4 import QtCore, QtGui
+from PyQt4.QtCore import QThread,SIGNAL
 import socket
 from struct import *
 import datetime
 import pcapy
 import sys
 import time
-import threading
 
-Row=0
+
+class getPacketsThread(QThread) :
+    def __init__(self,cap) :
+        QThread.__init__(self)
+        self.cap = cap
+
+    def __del__(self) :
+        self.wait()
+
+    def _get_top_packet(self) :
+        (header,packet) = cap.next()
+        protocol,src_mac,dest_mac = parse_packet(packet)
+        param = [protocol,src_mac,dest_mac]
+        return param
+
+    def run(self) :
+           for i in range(10) :
+                param = self._get_top_packet()
+                self.emit(SIGNAL('add_packet(QString)'),param[1])
+                self.sleep(1)
+
+
+                
+Row = 0
+
+
+
 try:
     _encoding = QtGui.QApplication.UnicodeUTF8
     def _translate(context, text, disambig):
@@ -19,6 +45,8 @@ try:
 except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
+
+
 
 def selectTrigger():
     if(ui_home.DeviceList.currentRow()==-1):
@@ -29,19 +57,26 @@ def selectTrigger():
         SelectedDevice=ui_home.DeviceList.currentItem().text()
         HomeWindow.close()
         MainWindow.show()
+        get_thread.start()
 
-def statueStart():
+
+def statueStart(packet=None):
+    global Row
     ui_main.statusBar.showMessage("Sniffing...")
     sniffing = True
-    cap = pcapy.open_live( str(SelectedDevice), 65536 , 1 , 0)
-    for i in range(10):
-        time.sleep(0.2)
-        (header, packet) = cap.next()
-        t = threading.Thread(target=parse_packet, args = (packet,))
-        t.daemon = True
-        t.start()
-        #t.join()
-        #parse_packet(packet)
+    item_0 = QtGui.QTreeWidgetItem(ui_main.PacketTable)
+    ui_main.PacketTable.topLevelItem(Row).setText(0, _translate("MainWindow", str(packet), None))
+    Row+=1
+    
+
+def statueStarts(packet=None):
+    
+    ui_main.statusBar.showMessage("Sniffing...")
+    sniffing = True
+    
+
+
+       
     
 
 def eth_addr (a) :
@@ -54,24 +89,26 @@ def parse_packet(packet) :
     eth_header = packet[:eth_length]
     eth = unpack('!6s6sH' , eth_header)
     eth_protocol = socket.ntohs(eth[2])
+    return eth_protocol,eth_addr(packet[0:6]),eth_addr(packet[6:12])
     #rowPosition = ui_main.PacketTable.rowCount()
     #ui_main.PacketTable.insertRow(rowPosition)
     #ui_main.PacketTable.setItem(rowPosition,0,QtGui.QTableWidgetItem(str(eth_protocol)))
     #ui_main.PacketTable.setItem(rowPosition , 1, QtGui.QTableWidgetItem(eth_addr(packet[0:6])))
     #ui_main.PacketTable.setItem(rowPosition , 2, QtGui.QTableWidgetItem(eth_addr(packet[6:12])))
-    item_0 = QtGui.QTreeWidgetItem(ui_main.PacketTable)
-    ui_main.PacketTable.topLevelItem(Row).setText(0, _translate("MainWindow", str(eth_protocol), None))
-    ui_main.PacketTable.topLevelItem(Row).setText(1, _translate("MainWindow", eth_addr(packet[0:6]), None))
-    ui_main.PacketTable.topLevelItem(Row).setText(2, _translate("MainWindow", eth_addr(packet[6:12]), None))
-    Row+=1
+
+
+    # item_0 = QtGui.QTreeWidgetItem(ui_main.PacketTable)
+    # ui_main.PacketTable.topLevelItem(Row).setText(0, _translate("MainWindow", str(eth_protocol), None))
+    # ui_main.PacketTable.topLevelItem(Row).setText(1, _translate("MainWindow", eth_addr(packet[0:6]), None))
+    # ui_main.PacketTable.topLevelItem(Row).setText(2, _translate("MainWindow", eth_addr(packet[6:12]), None))
+    # Row+=1
+
+
     #self.PacketTable.topLevelItem(rowPosition).setText(3, _translate("MainWindow", "field3", None))
     #elf.PacketTable.topLevelItem(rowPosition).setText(4, _translate("MainWindow", "field4", None))
 
     #ui_main.PacketTable.setItem(rowPosition , 3, QtGui.QTableWidgetItem(eth_protocol))
     #sys.exit(1)
-
-
-
 
     
 def statueStop():
@@ -93,7 +130,7 @@ def FilterFn():
         FilterFn()
     #msgBox.warning(ui_home.widget, "Alarm", word)
 
-
+cap = pcapy.open_live( "wlp8s0", 65536 , 1 , 0)
 sniffing = False
 app = QtGui.QApplication(sys.argv)
 HomeWindow = QtGui.QMainWindow()
@@ -102,6 +139,7 @@ ui_home = Ui_HomeWindow()
 ui_main = Ui_MainWindow()
 ui_home.setupUi(HomeWindow)
 ui_main.setupUi(MainWindow)
+
 if os.getuid()!=0 :
     msgBox = QtGui.QMessageBox()
     msgBox.warning(ui_home.widget, "not sudo", "please re-execute the app in admin(root) mode...")
@@ -109,8 +147,15 @@ if os.getuid()!=0 :
 devices = findalldevs()                  #list of strings to test addItems() function
 ui_home.DeviceList.addItems(devices)                #adds elemnts of the list(devices) to QlistWidget
 ui_home.select.clicked.connect(selectTrigger)       #when button (select) is been trigger it calls selectTrigger()
-ui_main.StartButton.clicked.connect(statueStart)
+ui_main.StartButton.clicked.connect(statueStarts)
 ui_main.StopSniffing.clicked.connect(statueStop)
 ui_main.FilterBtn.clicked.connect(FilterFn)
 HomeWindow.show()
+
+get_thread = getPacketsThread(cap)
+MainWindow.connect(get_thread,SIGNAL("add_packet(QString)"),statueStart)
+
+
+
+
 sys.exit(app.exec_())
---
+++ bucky_example.py
@@ -0,0 +1,26 @@
+import socket
+import textwrap
+
+
+def get_mac_addr(mac_raw):
+    byte_str = map('{:02x}'.format, mac_raw)
+    mac_addr = ':'.join(byte_str).upper()
+    return mac_addr
+
+# Formats multi-line data
+def format_multi_line(prefix, string, size=80):
+    size -= len(prefix)
+    if isinstance(string, bytes):
+        string = ''.join(r'\x{:02x}'.format(byte) for byte in string)
+        if size % 2:
+            size -= 1
+    return '\n'.join([prefix + line for line in textwrap.wrap(string, size)])
+
+
+class Ethernet:
+    def __init__(self, raw_data):
+        dest, src, prototype = struct.unpack('! 6s 6s H', raw_data[:14])
+        self.dest_mac = get_mac_addr(dest)
+        self.src_mac = get_mac_addr(src)
+        self.proto = socket.htons(prototype)
+        self.data = raw_data[14:]
